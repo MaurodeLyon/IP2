c01167a074139ca1d0caa5b66412ef15aeed5cb5
Mauro de Lyon <maurodelyon@gmail.com>
2015-11-02 14:50:23 +0100

    V1

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Simulator
@@ -10,6 +12,16 @@ namespace Simulator
     {
         static void Main(string[] args)
         {
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine("Availabe Comports: \n" + String.Join(" \n", ports));
+            Console.WriteLine("Enter Comport:");
+            string port = Console.ReadLine();
+            new FietsSimulator(port);
+            Console.WriteLine("Started Simulator");
+            while (true)
+            {
+                Thread.Sleep(10);
+            }
         }
     }
 }
