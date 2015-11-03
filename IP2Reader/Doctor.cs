27b365913da97faf7e431fe1d9bede1255fd1d24
Mauro de Lyon <maurodelyon@gmail.com>
2015-11-04 00:54:02 +0100

    start of for

---
+++ IP2Reader/Doctor.cs
@@ -0,0 +1,32 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP2Reader
+{
+    public partial class Doctor : Form
+    {
+        private List<Patient> patiens;
+        private Connect connect;
+
+        public Doctor()
+        {
+            InitializeComponent();
+            patiens = new List<Patient>();
+            connect = new Connect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connect.RequestData();
+            //this.chart1.Series["RPM"].Points.AddXY()
+        }
+    }
+}
---
+++ IP2Reader/connect.cs
@@ -0,0 +1,45 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IP2Reader
+{
+    class Connect
+    {
+        private TcpClient tcpClient;
+        private static string serverIP = "127.0.0.1";
+        private static int serverPort = 8800;
+        private List<Patient> patiens;
+
+        public Connect()
+        {
+            new Thread(StartServerConnection).Start();
+        }
+
+        private void StartServerConnection()
+        {
+            do { tcpClient = new TcpClient(serverIP, serverPort); }
+            while (!tcpClient.Connected);
+        }
+
+        public void RequestData()
+        {
+            NetworkCommunication.WriteMessage(tcpClient, "2");
+
+            //tiens = NetworkCommunication.ReceivePatients();
+        }
+
+        /*private List<Patient> RetreiveData()
+        {
+            while (tcpClient.Connected)
+            {
+                tcpClient.GetStream();
+            }
+        }*/
+    }
+}
