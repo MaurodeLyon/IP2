29ee686b36f2a9199130bfcce9a8f166c69b6646
Tom Remeeus <info@computerinfor.nl>
2015-11-03 19:46:24 +0100

    Added server and client

---
+++ IP2Server/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using System.Threading;
+
+namespace IP2Server
+{
+    class Client
+    {
+        private TcpClient client;
+
+        public Client(TcpClient client)
+        {
+            this.client = client;
+        }
+
+        private void incommingData()
+        {
+            Thread getData = new Thread(() =>
+            {
+                while(client.Connected)
+                {
+                    ProtocolHandler(NetworkCommunication.ReadMessage(client));
+                }
+            });
+
+            getData.Start();
+        }
+
+        private void ProtocolHandler(string data)
+        {
+            //protocolshizzle
+        }
+    }
+}
---
+++ IP2Server/Server.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+
+namespace IP2Server
+{
+    class Server
+    {
+        private static int Port = 8800;
+        private static TcpListener serverListener;
+        private List<TcpClient> users;
+
+        public Server()
+        {
+            serverListener = new TcpListener(IPAddress.Any, Port);
+            users = new List<TcpClient>();
+            GetConnections();
+        }
+
+        private void GetConnections()
+        {
+            try
+            {
+                Thread getConnections = new Thread(() =>
+                {
+                    serverListener.Start();
+                    Console.WriteLine("Server gestart, wachten op verbindingen...");
+
+                    while(true)
+                    {
+                        users.Add(serverListener.AcceptTcpClient());
+                        Console.WriteLine("Nieuwe verbinding geaccepteerd \r\n");
+                    }
+                });
+
+                getConnections.Start();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
